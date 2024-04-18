482f9778f2965c4fdc9307f493768aad2d82c874
akozubski@comperia.pl <akozubski@comperia.pl>
2024-04-18 13:22:48 +0200

    Update Response class - replace match construction with switch/case (source code compatibility with PHP 7.1 - workaround for Rector transpilation bug).

--- src/Api/Response.php
+++ src/Api/Response.php
@@ -39,29 +39,39 @@ abstract class Response
         }
 
         if ($response->getStatusCode() >= 400) {
-            throw match ($response->getStatusCode()) {
-                400 => new RequestValidationError(
-                    $this->getErrorMessage(
-                        $deserializedResponseBody,
+            switch ($response->getStatusCode()) {
+                case 400:
+                    throw new RequestValidationError(
+                        $this->getErrorMessage(
+                            $deserializedResponseBody,
+                            "Invalid request data: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
+                        )
+                    );
+
+                case 401:
+                    throw new AuthorizationError(
+                        $this->getErrorMessage(
+                            $deserializedResponseBody,
+                            "Invalid credentials: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
+                        )
+                    );
+
+                case 402:
+                case 403:
+                case 404:
+                case 405:
+                    throw new AccessDenied(
+                        $this->getErrorMessage(
+                            $deserializedResponseBody,
+                            "Access denied: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
+                        )
+                    );
+
+                default:
+                    throw new RequestValidationError(
                         "Invalid request data: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
-                    )
-                ),
-                401 => new AuthorizationError(
-                    $this->getErrorMessage(
-                        $deserializedResponseBody,
-                        "Invalid credentials: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
-                    )
-                ),
-                402, 403, 404, 405 => new AccessDenied(
-                    $this->getErrorMessage(
-                        $deserializedResponseBody,
-                        "Access denied: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
-                    )
-                ),
-                default => new RequestValidationError(
-                    "Invalid request data: {$response->getReasonPhrase()} [{$response->getStatusCode()}]"
-                ),
-            };
+                    );
+            }
         }
 
         if (($errorMessage = $this->getErrorMessage($deserializedResponseBody)) !== null) {
